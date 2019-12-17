3f2161ac6ff11a79e676f6638718e2f76f3a57af
xavren <renaudin.xavier@gmail.com>
2019-12-17 09:06:04 +0100

    Isolate tokens by USER_ID

--- generated/Endpoint/ListTilks.php
+++ generated/Endpoint/ListTilks.php
@@ -24,6 +24,8 @@ class ListTilks extends \Jane\OpenApiRuntime\Client\BaseEndpoint implements \Jan
      * @param array $headerParameters {
      *
      *     @var string $x-tilk-ref Identification de l'outil, peux contenir un numéro de version par ex. tool-2.1
+     *     @var int $USER_ID Tilkee user id
+     *     @var string $USER_EMAIL Tilkee user email
      * }
      */
     public function __construct(array $queryParameters = [], array $headerParameters = [])
@@ -77,10 +79,12 @@ class ListTilks extends \Jane\OpenApiRuntime\Client\BaseEndpoint implements \Jan
     protected function getHeadersOptionsResolver(): \Symfony\Component\OptionsResolver\OptionsResolver
     {
         $optionsResolver = parent::getHeadersOptionsResolver();
-        $optionsResolver->setDefined(['x-tilk-ref']);
+        $optionsResolver->setDefined(['x-tilk-ref', 'USER_ID', 'USER_EMAIL']);
         $optionsResolver->setRequired([]);
         $optionsResolver->setDefaults([]);
         $optionsResolver->setAllowedTypes('x-tilk-ref', ['string']);
+        $optionsResolver->setAllowedTypes('USER_ID', ['int']);
+        $optionsResolver->setAllowedTypes('USER_EMAIL', ['string']);
 
         return $optionsResolver;
     }
--- generated/Endpoint/UpdateProjectTilk.php
+++ generated/Endpoint/UpdateProjectTilk.php
@@ -15,6 +15,8 @@ class UpdateProjectTilk extends \Jane\OpenApiRuntime\Client\BaseEndpoint impleme
      * @param array $headerParameters {
      *
      *     @var string $x-tilk-ref Identification de l'outil, peux contenir un numéro de version par ex. tool-2.1
+     *     @var int $USER_ID Tilkee user id
+     *     @var string $USER_EMAIL Tilkee user email
      * }
      */
     public function __construct(int $projectId, int $id, \HbsResearch\Tilkee\API\Model\ProjectsProjectIdTokensIdPutBody $requestBody, array $headerParameters = [])
@@ -54,10 +56,12 @@ class UpdateProjectTilk extends \Jane\OpenApiRuntime\Client\BaseEndpoint impleme
     protected function getHeadersOptionsResolver(): \Symfony\Component\OptionsResolver\OptionsResolver
     {
         $optionsResolver = parent::getHeadersOptionsResolver();
-        $optionsResolver->setDefined(['x-tilk-ref']);
+        $optionsResolver->setDefined(['x-tilk-ref', 'USER_ID', 'USER_EMAIL']);
         $optionsResolver->setRequired([]);
         $optionsResolver->setDefaults([]);
         $optionsResolver->setAllowedTypes('x-tilk-ref', ['string']);
+        $optionsResolver->setAllowedTypes('USER_ID', ['int']);
+        $optionsResolver->setAllowedTypes('USER_EMAIL', ['string']);
 
         return $optionsResolver;
     }
